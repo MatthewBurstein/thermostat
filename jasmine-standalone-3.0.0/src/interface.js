1ebbed1d7adb1ff5fbab360c0684b8677654d0b1
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-22 14:51:03 +0000

    Add current temperature and reset functionality to page

--- jasmine-standalone-3.0.0/src/interface.js
+++ jasmine-standalone-3.0.0/src/interface.js
@@ -1,15 +1,26 @@
 $( document ).ready(function() {
   var thermostat = new Thermostat;
-  $('.mode_information').text(thermostat.displayMode);
-  $('.energy_usage_information').text(thermostat.energyUsage);
+
+  updateModeInformation()
+  updateTemperature()
+  $('.energy_usage_information').text(thermostat.energyUsage());
+
   $('#switch_mode_button').click(function() {
     thermostat.togglePowerSavingMode();
     updateModeInformation();
-
   });
 
+  $('#reset_button').click(function() {
+    thermostat.reset()
+    updateTemperature()
+  })
+
   function updateModeInformation() {
-    $('.mode_information').text(thermostat.displayMode);
+    $('.mode_information').text(thermostat.displayMode());
   };
+
+  function updateTemperature() {
+    $('.number').text(thermostat.temperature)
+  }
 
 });
